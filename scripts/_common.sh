aed7c6fd096a419348f79d2aab773910129b8a5f
matlag <matlag@chtisurel.net>
2020-11-19 15:52:40 -0500

    Use PHP 7.4

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@
 # dependencies used by the app
 pkg_dependencies="postgresql postgresql-contrib apt-transport-https"
 
-YNH_PHP_VERSION="7.3"
+YNH_PHP_VERSION="7.4"
 
 extra_php_dependencies="php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-pgsql php${YNH_PHP_VERSION}-xml"
 
